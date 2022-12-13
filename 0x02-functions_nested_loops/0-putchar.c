afea88c1f2e5129ce994ff95b845b491f1cbcf2e
Victor-elumah <Victorgodspowerelumah@gmail.com>
2022-12-13 12:28:28 -0800

    “0-putchar.c”

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and a new line
+ * main - Entry point
+ *
  * Return: Always 0
  */
+
 int main(void)
+
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
