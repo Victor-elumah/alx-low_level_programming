e0b6b288bbd7ef6425b3791b1e7be37aa845bbd6
Victor-elumah <Victorgodspowerelumah@gmail.com>
2022-12-13 08:26:47 -0800

    “8-print_base16.c”

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	while
+		(d < 10) {
+			puthcar(d + '0');
+			d++
+		}
+	while
+		(c <= 'f'); {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
