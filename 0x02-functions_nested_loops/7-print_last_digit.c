daebd5acd0b57a7865943589ac6a093ef35f17ee
Victor-elumah <Victorgodspowerelumah@gmail.com>
2022-12-14 12:29:49 -0800

    “7-print_last_digit.c”

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit
+ * print_ last_digit - prints the last digit
  * @n: the number
  * Return: value of the last digit
  */
-int print_last_digit(int n)
+int print_ last_digit(int n)
 {
 	int ld = n % 10;
 
