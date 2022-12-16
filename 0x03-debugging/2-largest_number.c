47f2b8fb40a9f3662590c8104ce7552f82b056fb
Victor-elumah <Victorgodspowerelumah@gmail.com>
2022-12-16 09:05:18 -0800

    “2-largest_number.c”

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,20 +13,19 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && a >= c)
+	if (a > b && a > c)
 	
 	{
 		largest = a;
 	}
 
-	else if (b >= a && b >= c)
+	else if (b > a && b > c)
 
 	{
 		largest = b;
 	}
 
-	esle if (c >= a && c >= b)
-
+	esle
 	{
 		largest = c;
 	}
